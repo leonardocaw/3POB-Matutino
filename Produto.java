b0710af6fa6a59fdebcc16295612d763fe7ddd24
leonardocaw <101294364+leonardocaw@users.noreply.github.com>
2024-05-09 10:21:38 -0300

    Create Produto.java

---
+++ Produto.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Produto {
+    private int id;
+    private String codigoBarras;
+    private String sku;
+    private String nome;
+    private String descricao;
+    private String categoria;
+    private double preco;
+    private double peso;
+    private String fabricante;
+
+    // Construtor
+    public Produto(int id, String codigoBarras, String sku, String nome, String descricao, String categoria, double preco, double peso, String fabricante) {
+        this.id = id;
+        this.codigoBarras = codigoBarras;
+        this.sku = sku;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.categoria = categoria;
+        this.preco = preco;
+        this.peso = peso;
+        this.fabricante = fabricante;
