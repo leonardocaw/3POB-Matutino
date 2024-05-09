d86027a25696c2b25fbe09e3baf43467e8918bbe
leonardocaw <101294364+leonardocaw@users.noreply.github.com>
2024-05-09 11:10:30 -0300

    Update Produto.java

--- Produto.java
+++ Produto.java
@@ -1,10 +1,12 @@
+package crud;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 class Produto {
     private int id;
-    private String codigoBarras;
+    private String codigoB;
     private String sku;
     private String nome;
     private String descricao;
@@ -13,11 +15,9 @@ class Produto {
     private double peso;
     private String fabricante;
 
-    
-
-    public Produto(int id, String codigoBarras, String sku, String nome, String descricao, String categoria, double preco, double peso, String fabricante) {
+    public Produto(int id, String codigoB, String sku, String nome, String descricao, String categoria, double preco, double peso, String fabricante) {
         this.id = id;
-        this.codigoBarras = codigoBarras;
+        this.codigoB = codigoB;
         this.sku = sku;
         this.nome = nome;
         this.descricao = descricao;
@@ -27,7 +27,80 @@ class Produto {
         this.fabricante = fabricante;
 
         public String toString() {
-        return "ID: " + id + ", Código de Barras: " + codigoBarras + ", SKU: " + sku + ", Nome: " + nome + ", Descrição: " + descricao + ", Categoria: " + categoria + ", Preço: " + preco + ", Peso: " + peso + ", Fabricante: " + fabricante;
+        return "ID: " + id + ", Código de Barras: " + codigoB + ", SKU: " + sku + ", Nome: " + nome + ", Descrição: " + descricao + ", Categoria: " + categoria + ", Preço: " + preco + ", Peso: " + peso + ", Fabricante: " + fabricante;
     }
 }
 
+	public int getId() {
+		return id;
+	}
+
+	public String getCodigoB() {
+		return codBarras;
+	}
+
+	public String getSku() {
+		return sku;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setCodBarras(String codBarras) {
+		this.codBarras = codBarras;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+}
